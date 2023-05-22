3a9417d71460f9a511b4a8208814318abf44c233
gmateu <gmateu@esliceu.com>
2023-05-22 10:47:33 +0200

    working with react, and python

---
+++ package_installatino_react.sh
@@ -0,0 +1,9 @@
+#\!/bin/bash
+yarn create vite .
+yarn --exact
+#yarn dev to run server
+sudo sysctl fs.inotify.max_queued_events=16384
+sudo sysctl fs.inotify.max_user_instances=8192
+sudo sysctl fs.inotify.max_user_watches=524288
+#yarn dev --host
+npx tailwindcss init -p
