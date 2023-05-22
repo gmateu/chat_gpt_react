ce03314c031d1c1612b1b2ceadbe5cc6ba553df9
gmateu <gmateu@esliceu.com>
2023-05-22 16:35:39 +0200

    preparant backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+#main imports
+from fastapi import FastAPI,File,UploadFile,HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
 
+#custom Function imports
+
+
+#initiate App
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello d fsd world!"}
+
+#CORS - Origins
+origins =[
+    "http://192.168.1.10:5173",
+    "http://192.168.1.10:5174",
+]
+
+#CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/health")
+async def check_health():
+    return {"message": "healthy"}
--- backend/run_uvicorn.sh
+++ backend/run_uvicorn.sh
@@ -1,2 +1,3 @@
-#\!/bin/bash
+#!/bin/bash
+source venv/bin/activate
 uvicorn --host 0.0.0.0 main:app --reload
