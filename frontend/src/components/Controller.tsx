6ad409dd40c5051d49df2961ec7ff30786d161bd
gmateu <gmateu@esliceu.com>
2023-05-27 07:45:45 +0200

    no es dispara el handleStop

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -6,19 +6,22 @@ function Controller() {
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<any[]>([]);
 
-    const createBlobUrl = (data: any) =>{};
-
-    const handleStop = async () => {
+  const createBloUrl = (data: any) =>{  };
+  const handleStop = async () => {
+      alert('Stopping');
 
     }
   return (
     <div className='h-screen overflow-y-hidden'>
         <Title setMessages = {setMessages}/>
         <div className='flex felx-col justify-between h-full overflow-y-scroll pb-96'>
+
+
+
             {/* Recorder */}
             <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
               <div className="flex justify-center items-center w-full">
-                <RecordMessage handleStop={handleStop}/>
+              <RecordMessage handleStop={handleStop} />
               </div>
             </div>
         </div>
